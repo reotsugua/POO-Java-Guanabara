c69f31edcdd8dacd9a3ca090030a95d1e932674b
reotsugua <renanaugusto.ras@gmail.com>
2023-01-16 21:57:36 -0300

    Aula 11 Herança

---
+++ PooJava/src/Aula11/Bolsista.java
@@ -0,0 +1,12 @@
+package Aula11;
+
+public class Bolsista extends Aluno{
+    private int bolsa;
+
+    public void renovarBolsa(){}
+
+    @Override
+    public void pagarMensalidade() {
+        super.pagarMensalidade();
+    }
+}
