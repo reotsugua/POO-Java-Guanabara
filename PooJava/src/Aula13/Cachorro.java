b5ba7fcf639b5e02f7e747b46f6a0dc279dd6212
reotsugua <renanaugusto.ras@gmail.com>
2023-01-23 23:05:57 -0300

    Aula 13 Primeiro Finalizando Polimorfismo de Sobrecarga

--- PooJava/src/Aula13/Cachorro.java
+++ PooJava/src/Aula13/Cachorro.java
@@ -1,15 +1,53 @@
 package Aula13;
 
+import java.util.Objects;
+
 public class Cachorro extends Lobo{
     @Override
     public void emitirSom() {
         System.out.println("Au au au au");
     }
 
-    public void reagir(String frase){}
-    public void reagir(int hora, int min){}
-    public void reagir(boolean dono){}
-    public void reagir(int idade, float peso){}
+    public void reagir(String frase){
+        if (Objects.equals(frase, "Toma Comida") || frase.equals("Olá")) {
+            System.out.println("Abanar e Latir");
+        } else {
+            System.out.println("Rosnar");
+        }
+    }
+    public void reagir(int hora, int min){
+        if (hora < 12) {
+            System.out.println("Abanar");
+        } else if (hora >= 18) {
+            System.out.println("Ignorar");
+        } else {
+            System.out.println("Abanar e Latir");
+        }
+    }
+    public void reagir(boolean dono){
+        if (dono) {
+            System.out.println("Abanar");
+        } else {
+            System.out.println("Rosnar e Latir");
+            this.emitirSom();
+        }
+    }
+    public void reagir(int idade, float peso){
+        if (idade < 5) {
+            if (peso < 10) {
+                System.out.println("Abanar");
+            } else {
+                System.out.println("Latir");
+            }
+        } else {
+            if (peso < 10) {
+                System.out.println("Rosnar");
+            }else {
+                System.out.println("Ignorar");
+            }
+        }
+
+    }
 
 
 }
--- PooJava/src/Aula13/Main.java
+++ PooJava/src/Aula13/Main.java
@@ -2,7 +2,8 @@ package Aula13;
 
 public class Main {
     public static void main(String[] args) {
-        Cachorro x = new Cachorro();
-        x.emitirSom();
+        Cachorro c = new Cachorro();
+        c.reagir(true);
+        c.reagir(false);
     }
 }
